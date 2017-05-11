47a395f751619ad725ebfdebad2bd6804b2a7c73
Javier <jvrcueto@outlook.com>
2017-05-11 02:47:21 -0400

    solved issue with faulty filepaths in index.ejs, also changed the colors (again)

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(layouts);
+// app.use(layouts);
 
 const index = require('./routes/index');
 app.use('/', index);
