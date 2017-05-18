aa40cc5d7ed48fe815aeae2efbe48aff3276046a
Javier <jvrcueto@outlook.com>
2017-05-18 16:21:36 -0400

    trying to make the images inside the carousel scale based on the longest side, similar to video players' theater backgrounds

--- public/javascripts/grayscale.js
+++ public/javascripts/grayscale.js
@@ -62,3 +62,32 @@ $(function() {
 $('.navbar-collapse ul li a').click(function() {
   $(".navbar-collapse").collapse('hide');
 });
+
+//Resize project images
+const jCarousel = $('#myCarousel')
+const jTango = $('#tango-img');
+
+const scaleWithVWidth  = (jel) => jel.width ('100vw').height('auto');
+const scaleWithVHeight = (jel) => jel.width('auto').height('100vh');
+const imgAspectRatio = () => jTango.width() / jTango.height();
+
+//Strategy (keeping the aspect ratio the same)
+//window is wide and image would fit entirely:    scaleWithVHeight
+//window is wide but image would be clipped:      scaleWithVWidth
+//window is tall and image would fit entirely:    scaleWithVWidth
+//window is tall but image would be clipped:      scaleWithVHeight
+//Note: this code /probably/ does not work with vertical videos yet
+const resizeImgToFitWindow = (jImg) => {
+  const winWidth = jWin.width();
+  const winHeight = jWin.height();
+  if (winWidth >  winHeight && imgAspectRatio() * winHeight < winWidth) {
+      scaleWithVHeight(jImg);
+    }
+  else {
+    scaleWithVWidth(jImg);
+  }
+
+  shrinkBar();
+};
+
+jWin.resize(resizeImgToFitWindow(jTango));
