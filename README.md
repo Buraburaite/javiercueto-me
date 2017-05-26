950baeabe5fd55f3f16e8e1ed840d5d99031e53c
Javier <jvrcueto@outlook.com>
2017-05-26 16:30:00 -0400

    updating readme, improving commentation

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portfolio site for Javier Cueto, hosted at [javiercueto.me](http://javiercueto.me)
--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const favicon      = require('serve-favicon');
 const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
-const layouts      = require('express-ejs-layouts');
 const mongoose     = require('mongoose');
 
-require('dotenv').config(); //access environmental variables
+// Access environmental variables
+require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// default value for title local
-app.locals.title = 'Javier Cueto - portfolio';
+// Default value for title local
+app.locals.title = 'Javier Cueto - Web Developer';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,25 +26,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(layouts);
 
 const index = require('./routes/index');
 app.use('/', index);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// Error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
