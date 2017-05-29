90216cb1a139ca126ae935f0b9496fdf5b56a72f
Javier <jvrcueto@outlook.com>
2017-05-28 23:16:41 -0700

    converting to es6

--- public/javascripts/grayscale.js
+++ public/javascripts/grayscale.js
@@ -7,52 +7,52 @@
 // Modifications and additions have been made by Javier Cueto, 2017
 // Hosted at javiercueto.me
 
-var jWin = $(window);
-var navbar = $('.navbar');
+const jWin = $(window);
+const navbar = $('.navbar');
 // collapse the navbar on scroll
-function collapseNavbar() {
+const collapseNavbar = () => {
   if (navbar.offset().top > 50) {
     navbar.addClass("top-nav-collapse");
   } else {
     navbar.removeClass("top-nav-collapse");
   }
-}
+};
 jWin.scroll(collapseNavbar);
 
 // On document.ready...
-$(function() {
+$(() => {
 
   // ...collapse the navbar, if necessary...
   collapseNavbar();
 
   // ...turn all page-scroll anchors into smooth scrolling buttons...
-  $('a.page-scroll').bind('click', function(event) {
+  $('a.page-scroll').bind('click', (e) => {
 
-    var targetDiv = $($(this).attr('href'));
-    var navHeight = 50;
-    var scrollToVal = targetDiv.offset().top +
+    const targetDiv = $($(this).attr('href'));
+    const navHeight = 50;
+    const scrollToVal = targetDiv.offset().top +
     parseInt(targetDiv.css('padding-top'))
     - navHeight;
 
     $('html, body').stop().animate({
       scrollTop: scrollToVal
     }, 1200, 'easeInBack');
-    event.preventDefault();
+    e.preventDefault();
   });
 
   /*====
   If the contents of a non-text section (i.e. a .grey-section's non-text
   contents) is too large to fit in the window, then shrink it to fit
   ====*/
-  function shrinkToFit() {
+  const shrinkToFit = () => {
 
-    var maxAllowableHeight = jWin.height() * 0.7;
+    const maxAllowableHeight = jWin.height() * 0.7;
     $('.shrink-to-fit').each(function() {
-      var shrinkMe = $(this);
-      var origHeight = shrinkMe.height();
-      var origWidth  = shrinkMe.width();
+      const shrinkMe = $(this);
+      const origHeight = shrinkMe.height();
+      const origWidth  = shrinkMe.width();
       if (origHeight > maxAllowableHeight) {
-        var shrinkScale = maxAllowableHeight / origHeight;
+        const shrinkScale = maxAllowableHeight / origHeight;
         shrinkMe.height(origHeight * shrinkScale);
         shrinkMe.width (origWidth  * shrinkScale);
       }
@@ -63,24 +63,24 @@ $(function() {
   jWin.resize(shrinkToFit);
 
   // ...reveal .content-section divs as the user scrolls down to them...
-  var totalDivsToReveal = $('.content-section').length;
-  var revealedDivs = [];
-  var approachDistance = jWin.height() * 0.4;
+  const totalDivsToReveal = $('.content-section').length;
+  const revealedDivs = [];
+  const approachDistance = jWin.height() * 0.4;
 
 
   // ...whenever scrolling happens...
-  jWin.scroll(function() {
+  jWin.scroll(() => {
     // ...check if there are still unrevealed divs...
     if (totalDivsToReveal > revealedDivs.length) {
       // ...if so, then for each div...
-      $('.content-section').each(function() {
-        var targetDiv = $(this);
-        var id = targetDiv.attr('id');
+      $('.content-section').each(() => {
+        const targetDiv = $(this);
+        const id = targetDiv.attr('id');
         // ...if the div has yet to be revealed...
         if (!revealedDivs.includes(id)) {
           // ...check if the page has scrolled far enough down to reveal...
-          var targetDivTop = targetDiv.offset().top;
-          var winBottom = jWin.scrollTop() + jWin.height();
+          const targetDivTop = targetDiv.offset().top;
+          const winBottom = jWin.scrollTop() + jWin.height();
           // ...if so, then reveal the div as you approach it...
           if (winBottom > targetDivTop + approachDistance) {
             targetDiv.animate(
@@ -100,6 +100,6 @@ $(function() {
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(() => {
   $(".navbar-collapse").collapse('hide');
 });
